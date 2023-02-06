24afcbf09e9074d6f14117319a79e3da401f45cd
Linas Paulauskas <paulauskas.linas@gmail.com>
2023-02-06 07:34:18 +0200

    update html, convert styling to BEM

--- src/components-ui/footer/footer.js
+++ src/components-ui/footer/footer.js
@@ -2,27 +2,28 @@ import './footer.scss';
 
 function createFooter() {
     const footer = document.createElement('footer');
+    footer.classList.add('footer');
 
     const divGroup1 = document.createElement('div');
-    divGroup1.classList.add('footer-group');
+    divGroup1.classList.add('footer__left-group');
 
     const firstP = document.createElement('p');
     firstP.textContent = 'A project based on';
     
     const innerDivGroup1 = document.createElement('div');
-    innerDivGroup1.classList.add('odin-group');
+    innerDivGroup1.classList.add('footer__odin-group');
 
     const odinLink = document.createElement('a');
     odinLink.href = 'https://www.theodinproject.com/home';
     odinLink.target = '_blank';
 
     const odinImg = document.createElement('img');
-    odinImg.classList.add('odin-logo');
+    odinImg.classList.add('footer__odin-logo');
     odinImg.src = 'https://gist.githubusercontent.com/paulauskas-linas/8e086442e1a6223caef4792dbfce1288/raw/632a7a2f673b74238e8f95614d5ac365e8638b9f/odin.svg';
     odinImg.alt = 'logo of odin project';
 
-    const secondP = document.createElement('p');
-    secondP.classList.add('odin-text');
+    const secondP = document.createElement('span');
+    secondP.classList.add('footer__odin-text');
     const odinLink2 = document.createElement('a');
     odinLink2.href = 'https://www.theodinproject.com/home';
     odinLink2.target = '_blank';
@@ -41,13 +42,13 @@ function createFooter() {
     divGroup1.appendChild(innerDivGroup1);
 
     const divGroup2 = document.createElement('div');
-    divGroup2.classList.add('footer-group');
+    divGroup2.classList.add('footer__right-group');
 
-    const fourthP = document.createElement('p');
+    const fourthP = document.createElement('span');
     fourthP.textContent = ' By Linas Paulauskas';
 
     const profileDiv = document.createElement('div');
-    profileDiv.classList.add('profile-group');
+    profileDiv.classList.add('footer__profile-group');
 
     const profileLink = document.createElement('a');
     profileLink.classList.add('profile-link');
@@ -55,7 +56,7 @@ function createFooter() {
     profileLink.target = '_blank';
 
     const profileImg = document.createElement('img');
-    profileImg.classList.add('profile-img');
+    profileImg.classList.add('footer__profile-logo');
     profileImg.src = 'https://gist.githubusercontent.com/paulauskas-linas/5634d9c485f2d5670aab310d127ba085/raw/3737ef88d2c002b6579de37f0a1c89c19afa527b/GitHub-icon.svg';
     profileImg.alt = 'github logo';
 
