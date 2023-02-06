cd780a25aea97964096fda8780dd3e114576a992
Linas Paulauskas <paulauskas.linas@gmail.com>
2023-02-06 07:34:43 +0200

    preload footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import createFooter from './components-ui/footer/footer';
 import createHeader from './components-ui/header/header';
 
 const content = document.getElementById('content');
 
 content.appendChild(createHeader());
+content.appendChild(createFooter());
